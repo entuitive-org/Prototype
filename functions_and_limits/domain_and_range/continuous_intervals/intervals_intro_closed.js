1744691764e9d2172d398bfd69084bc12e70f236
Phil <91705643+philstrz@users.noreply.github.com>
2022-06-22 08:49:45 -0700

    Basic numberline
    Implemented basic numberline with intervals and points

--- functions_and_limits/domain_and_range/continuous_intervals/intervals_intro_closed.js
+++ functions_and_limits/domain_and_range/continuous_intervals/intervals_intro_closed.js
@@ -1,7 +1,16 @@
 window.addEventListener("load", int_intro_closed_setup, true);
+window.addEventListener("load", int_intro_open_setup, true);
 
 function int_intro_closed_setup() {
     canvas = document.getElementById("intervals_intro_closed")
 
-    let numberline = new Numberline(canvas);
+    let numberline = new Numberline(canvas, [0, 5]);
+    numberline.addSet([1, 3, [1,3]]);
+}
+
+function int_intro_open_setup() {
+    canvas = document.getElementById("intervals_intro_open")
+
+    let numberline = new Numberline(canvas, [0, 5]);
+    numberline.addSet([[1, 3]]);
 }
--- scripts/numberline.js
+++ scripts/numberline.js
@@ -31,28 +31,30 @@ class Numberline {
     }
 
     // Drawing params
-    static lineWidth = 12;
+    static lineWidth = 24;
     static axisWidth = 8;
-    static pointSize = 16;
+    static pointSize = 24;
+    static pointThickness = 8;
     static arrowSize = 32;
     static tickLength = 16;
     static font = "64px monospace";
-    static textOffset = 64;
+    static textOffset = 128;
     
+    // Force array to be read as floats
+    static parseArray(array) {
+        for (let i = 0; i < array.length; i++) {
+            array[i] = parseFloat(array[i]);
+            return array;
+        }
+    }
 
     constructor(canvas, xlim=[-10,10], independent='x') {
         // Get canvas and context
         this.canvas = canvas;
         this.ctx = this.canvas.getContext("2d");
 
-        // Force xlim array to be read as floats
-        function parseArray(array) {
-            for (let i = 0; i < array.length; i++) {
-                array[i] = parseFloat(array[i]);
-                return array;
-            }
-        }
-        this.lim = parseArray(xlim);
+        // Get limits and set scale
+        this.lim = Numberline.parseArray(xlim);
         this.scale = Numberline.width / (xlim[1] - xlim[0]);
 
         // Set variable name
@@ -67,6 +69,10 @@ class Numberline {
 
         // Draw and label axes
         this.drawAxes();
+
+        // Placeholders for intervals and points
+        this.points = [];
+        this.intervals = [];
     }
 
     drawAxes() {
@@ -110,26 +116,100 @@ class Numberline {
         right = Math.ceil(this.lim[1] - 1);
 
         let x = 0;
+        ctx.font = Numberline.font;
+        ctx.textAlign = "center";
+        ctx.textBaseline = "center";
+        let text = null;
         for (let i = left; i <= right; i++) {
             x = i * this.scale;
+            // Draw the tick mark
             ctx.beginPath();
             ctx.moveTo(x, -Numberline.tickLength);
             ctx.lineTo(x, Numberline.tickLength);
             ctx.stroke();
+            // Label it
+            text = i;
+            ctx.fillText(text, x, Numberline.textOffset);
         }
 
     }
 
     addPoint(x) {
-
+        let point = new Point(this, x, Numberline.colors.base);
+        point.draw();
+        this.points.push(point);
     }
 
     addInterval(a,b) {
-
+        let int = new Interval(this, a, b, Numberline.colors.base);
+        int.draw();
+        this.intervals.push(int);
     }
 
     addSet(array) {
+        // Sort array into points and intervals
+        for (let elem of array) {
+            switch (typeof(elem)) {
+                case 'number':
+                    this.addPoint(elem);
+                    break;
+                case 'object':
+                    elem = Numberline.parseArray(elem);
+                    this.addInterval(elem[0], elem[1]);
+                    break;
+            }
+        }
+    }
 
+    drawPoint(x, closed=true) {
+        let ctx = this.ctx;
+        ctx.beginPath();
+        ctx.arc(this.scale*x, 0, Numberline.pointSize, 0, 2 * Math.PI);
+        if (closed) {
+            ctx.fill();
+        } else {
+            ctx.lineWidth = Numberline.pointThickness;
+            ctx.stroke();
+        }
+    }
+
+    drawLine(a,b) {
+        let ctx = this.ctx;
+        ctx.lineWidth = Numberline.lineWidth;
+        ctx.beginPath();
+        ctx.moveTo(a*this.scale + Numberline.pointSize - Numberline.pointThickness, 0);
+        ctx.lineTo(b*this.scale - Numberline.pointSize + Numberline.pointThickness, 0);
+        ctx.stroke();
+        this.drawPoint(a, false);
+        this.drawPoint(b, false);
     }
 }
 
+class Interval {
+    constructor(numberline, a, b, color="#000") {
+        this.numberline = numberline;
+        this.a = a;
+        this.b = b;
+        this.color = color;
+    }
+
+    draw() {
+        this.numberline.ctx.strokeStyle = this.color;
+        this.numberline.ctx.fillStyle = this.color;
+        this.numberline.drawLine(this.a, this.b);
+    }
+}
+
+class Point {
+    constructor(numberline, x, color) {
+        this.numberline = numberline;
+        this.x = x;
+        this.color = color;
+    }
+
+    draw() {
+        this.numberline.ctx.strokeStyle = this.color;
+        this.numberline.ctx.fillStyle = this.color;
+        this.numberline.drawPoint(this.x);
+    }
+}
